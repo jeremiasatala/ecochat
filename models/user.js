4481099aea6ba44154325f3a81d2e2d005279f07
jeremiasatala <jeremiasatala@gmail.com>
2025-08-30 18:12:39 -0300

    Duerman a linus torvalds

--- models/user.js
+++ models/user.js
@@ -3,13 +3,8 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
-
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  avatar: { type: String, default: '' } // nuevo campo para la foto de perfil
+  avatar: { type: String, default: '' } // campo para la foto de perfil
 });
 
 // Antes de guardar, encriptar la contraseña
@@ -22,6 +17,6 @@ userSchema.pre('save', async function(next) {
 // Método para comparar contraseña
 userSchema.methods.comparePassword = function(password) {
   return bcrypt.compare(password, this.password);
-}
+};
 
 module.exports = mongoose.model('User', userSchema);
